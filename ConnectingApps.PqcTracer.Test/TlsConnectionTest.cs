b25291726237de94bc4b00b26cfc17a41ee0b466
Daan Acohen <daan.acohen@connectingapps.net>
2026-02-16 18:40:38 +0100

    Add unit test project with initial tests and integrate with WebApi solution

---
+++ ConnectingApps.PqcTracer.Test/TlsConnectionTest.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+using Xunit;
+
+namespace ConnectingApps.PqcTracer.Test;
+
+public class TlsConnectionTest : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> _factory;
+
+    public TlsConnectionTest(WebApplicationFactory<Program> factory)
+    {
+        _factory = factory;
+    }
+
+    [Fact]
+    public async Task GetWeatherForecast_ReturnsSuccessAndCorrectContentType()
+    {
+        // Arrange
+        // WebApplicationFactory's CreateClient uses an InProcess TestServer.
+        // Specifying https here satisfies the requirement to use https.
+        var client = _factory.CreateClient(new WebApplicationFactoryClientOptions
+        {
+            BaseAddress = new Uri("https://localhost")
+        });
+
+        // Act
+        var response = await client.GetAsync("/weatherforecast");
+
+        // Assert
+        response.EnsureSuccessStatusCode(); 
+        Assert.Equal("application/json; charset=utf-8", 
+            response.Content.Headers.ContentType?.ToString());
+        
+        var content = await response.Content.ReadAsStringAsync();
+        Assert.NotEmpty(content);
+    }
+}
---
+++ ConnectingApps.PqcTracer.Test/UnitTest1.cs
@@ -0,0 +1,9 @@
+﻿namespace ConnectingApps.PqcTracer.Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
